a9d0397b54760bda2cfd2f78a51fea8aefae702d
Friliat <Friliat@WIN-7P8G0LK8L3L>
2023-11-26 17:52:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double E = 0.001;
+            double S = 0;
+            int n = 0;
+            double x = Convert.ToDouble(Console.ReadLine());
+            double A = x;
+            do
+            {
+                S = S + A;
+                n++;
+                A = A * (n + 1) / ((n + 2) * n);
+            }
+            while (A >= E);
+            Console.WriteLine($"Сумма ряда S={S,8:f4} , Число итераций n={n}");
+        }
+    }
+}
